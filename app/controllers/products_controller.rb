efc9a02c6c63a88ff74b958cb0a845ae3d9969e9
Vicencio42 <vicencio42@gmail.com>
2022-03-08 20:00:15 -0600

    Updated controller and routes file

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,14 @@
 class ProductsController < ApplicationController
 
-  get "/one_product" controller: "products" action: "one_product_method"
-
-  get "/all_products" controller: "prodcuts" action: "all_products_method"
-  
+def one_product_method
+  product = Product.first
+  render json: product.as_json
 end
+
+def all_products_method
+  products = Product.all
+  render json: products.as_json
+end
+
+end
+
