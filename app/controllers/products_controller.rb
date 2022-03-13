980000c518d1a8f3ac314da542ab58220381ebdb
Vicencio42 <vicencio42@gmail.com>
2022-03-13 10:28:10 -0500

    Updated code to use RESTful conventions for index/show actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,20 +1,13 @@
 class ProductsController < ApplicationController
 
-def one_product_method
-  user_input = params["name"]
-  product = Product.find_by(name: user_input)
-  render json: product.as_json
-end
+  def index
+    products = Product.all
+    render json: products.as_json
+  end
 
-def second_product_method
-  user_input = params["name"]
-  product = Product.find_by(name: user_input)
+def show
+  product = Product.find_by(id: params["id"])
   render json: product.as_json
-end
-
-def all_products_method
-  products = Product.all
-  render json: products.as_json
 end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
+
+  get "/products" => "products#index"
  
-  get "/one_product" => "products#one_product_method"
-
-  get "/second_product/:name" => "products#second_product_method"
-
-  get "/all_products" => "products#all_products_method"
+  get "/products/:id" => "products#show"
+  
 
 end
