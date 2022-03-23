e7215304ad9ea7ccbbc5eb75889faa5ac8c71550
Vicencio42 <vicencio42@gmail.com>
2022-03-22 19:55:29 -0500

    added orders show and index action on controller, then added routes, and http requests

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,10 @@
 class OrdersController < ApplicationController
 
+  def index
+    orders = Order.all
+    render json: orders
+  end
+
   def create
     order = Order.new(
       user_id: params["user_id"],
@@ -14,6 +19,11 @@ class OrdersController < ApplicationController
     else
       render json: {error_message: product.errors.full_messages}, status: 422
     end
+  end
+
+  def show
+    order = Order.find_by(id: params["id"])
+    render json: order
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,7 @@ Rails.application.routes.draw do
 
 #orders
   post "/orders" => "orders#create"
+  get "/orders/:id" => "orders#show"
+  get "/orders" => "orders#index"
   
 end
