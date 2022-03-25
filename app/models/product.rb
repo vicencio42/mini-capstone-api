53419098f346abd92d005a6cd0854d99f3c36c90
Vicencio42 <vicencio42@gmail.com>
2022-03-24 19:47:54 -0500

    added the model assocaitions belongs to, has many

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,6 +8,7 @@ class Product < ApplicationRecord
   has_many :orders
   has_many :category_products
   has_many :categories, through: :category_products
+  has_many :carted_products
 
   def is_discounted?
     if price < 10
