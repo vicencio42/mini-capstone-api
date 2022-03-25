9c38b12ca821b58126e0fcca804134b3f9c191ba
Vicencio42 <vicencio42@gmail.com>
2022-03-24 21:03:02 -0500

    fixed has many belongs to assoiations

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,16 +1,17 @@
 class CartedProductsController < ApplicationController
+  before_action :authenticate_user
 
   def create
     carted_product = CartedProduct.new(
-      user_id: params["user"],
+      user_id: current_user.id,
       product_id: params["product_id"],
       quantity: params["quantity"],
       status: params["status"],
-      order_id: params["order_id"]
+      order_id: params["order_id"],
 
     )
     if carted_product.save #happy path
-      render json: {error_message: carted_product.erros.full_messages}, status: 422
+      render json: { error_message: carted_product.errors.full_messages }, status: 422
     end
+  end
 end
-
