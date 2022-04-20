f8fd171506f29b6018b025be57a45792177ac0de
Vicencio42 <vicencio42@gmail.com>
2022-04-20 18:47:38 -0500

    fixed cors file to make http requests

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -3,4 +3,45 @@ class SuppliersController < ApplicationController
     suppliers = Supplier.all
     render json: suppliers
   end
+
+  def show
+    supplier = Supplier.find_by(id: params["id"])
+    render json: supplier
+  end
+
+  def create
+    supplier = Supplier.new(
+      name: params["name"],
+      email: params["email"],
+      phone_number: params["phone_number"],
+    )
+
+    if supplier.save
+      render json: supplier
+    else
+      render json: { error_massages: supplier.errors.full_messages }, status: 422
+    end
+  end
+
+  def update
+    supplier_id = params["id"]
+    supplier = Supplier.find(supplier_id)
+
+    supplier.name = params["name"] || supplier.name
+    supplier.email = params["email"] || supplier.email
+    supplier.phone_number = params["phone_number"] || supplier.phone_number
+
+    if supplier.save
+      render json: supplier
+    else
+      render json: { error_massages: supplier.errors.full_messages }, status: 422
+    end
+  end
+
+  def destroy
+    supplier_id = params["id"]
+    supplier = Supplier.find_by(id: supplier_id)
+    supplier.destroy
+    render json: "Supplier has been terminated."
+  end
 end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "example.com", "mini-capstone.peterxjang.com"
+    origins "example.com", "mini-capstone.peterxjang.com", "localhost:8080"
 
     resource "*",
       headers: :any,
